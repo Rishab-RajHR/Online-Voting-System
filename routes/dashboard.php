519338ffe62ea2899985cbe3fc4eb76c26d0358b
Rishab Raj <rr0948555@gmail.com>
2026-07-13 14:55:49 +0530

    Voting System

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -7,6 +7,13 @@
     $userdata = $_SESSION['userdata'];
     $groupsdata = $_SESSION['groupsdata'];
 
+    if($userdata['status']==0){
+       $status = "<b style='color:red'>Not Voted</b>";
+    }
+    else{
+      $status = "<b style='color:green'>Voted</b>";
+    }
+
 ?>
 
 <html>
@@ -23,6 +30,7 @@
              background-color: #3498db;
              color: white;
              float: left;
+             margin: 10px;
            }
 
            #logoutBtn{
@@ -32,6 +40,7 @@
               background-color: #3498db;
               color: white;
               float: right;
+              margin: 10px;
            }
 
            #Profile{
@@ -78,7 +87,7 @@
               <b>Name:</b><?php echo $userdata['name']?><br><br>
               <b>Mobile:</b><?php echo $userdata['mobile']?><br><br>
               <b>Address:</b><?php echo $userdata['address']?><br><br>
-              <b>Status:</b><?php echo $userdata['status']?><br><br>
+              <b>Status:</b><?php echo $status ?><br><br>
            </div>
 
           <div id="Group">
@@ -91,8 +100,9 @@
                               <img style="font:right" src="../uploads/<?php echo $groupsdata[$i]['photo'] ?>" height="100" width="100">
                              <b>Group Name: </b><?php echo $groupsdata[$i]['name'] ?><br><br>
                              <b>Votes: </b><?php echo $groupsdata[$i]['votes'] ?><br><br>
-                             <form action="#">
-                                 <input type="hidden" name="gvotes" value=""><?php echo $groupsdata[$i]['votes'] ?>
+                             <form action="../api/vote.php" method="post">
+                                 <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
+                                 <input type="hidden" name="gid" value="<?php echo $groupsdata[$i]['id'] ?>">
                                  <input type="submit" name="votebtn" value="Vote" id="votebtn">
                              </form>
                           </div>
